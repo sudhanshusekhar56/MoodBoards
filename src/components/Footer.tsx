50c863bfcbad3b2c4205085be9929d2eecd81129
Sudhanshu Sekhar <sudhanshusekhar56@hotmail.com>
2025-01-02 00:15:22 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-500 text-white py-4">
+    <footer className="bg-gray-700 text-white py-4">
       <p className="text-center">&copy; {currentYear} Made with love ❤️</p>
     </footer>
   );
